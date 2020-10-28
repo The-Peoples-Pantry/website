e8e6efb52bd64b3bd6ab956b3af9cd72a74b5e61
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2020-10-27 20:50:34 -0400

    Activate django_heroku

--- website/website/settings.py
+++ website/website/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -119,3 +121,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
+
+# Configure hosted settings automatically using django_heroku
+django_heroku.settings(locals())
