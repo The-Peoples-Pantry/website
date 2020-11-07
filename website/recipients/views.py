604b2d56eed4a8dab050eb1c766dd1e44e554ce8
Eric Lindau <eric.lindau1@gmail.com>
2020-11-06 23:17:54 -0500

    Add basic request form submission handling and UUID generation

--- website/recipients/views.py
+++ website/recipients/views.py
@@ -1,4 +1,13 @@
+import uuid
 from django.shortcuts import render
+from django.forms import ModelForm
+from .models import MealRequest
+
+
+class MealRequestForm(ModelForm):
+    class Meta:
+        model = MealRequest
+        exclude = []
 
 
 def index(request):
@@ -6,4 +15,10 @@ def index(request):
 
 
 def new(request):
-    return render(request, 'recipients/new.html')
+    if request.method == 'POST':
+        form = MealRequestForm(request.POST)
+        request_uuid = uuid.uuid4()
+        if form.is_valid():
+            return render(request, 'recipients/confirmation.html', {"name": request.POST['name'], "uuid": request_uuid})
+
+    return render(request, 'recipients/new.html', {"request_form": MealRequestForm()})
