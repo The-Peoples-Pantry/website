bf65775a9e2749d1ccef69be9fa9a5a3d3d97eac
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2020-11-13 21:37:49 -0500

    Customize which columns are shown in the table

--- website/volunteers/tables.py
+++ website/volunteers/tables.py
@@ -5,4 +5,26 @@ from recipients.models import MealRequest
 class MealRequestTable(tables.Table):
     class Meta:
         model = MealRequest
-        attrs = {"class": "table-responsive"}
+        attrs = {"class": "table table-hover table-responsive"}
+        fields = (
+            'id',
+            'city',
+            'notes',
+            'num_adults',
+            'num_children',
+            'children_ages',
+            'food_allergies',
+            'food_preferences',
+            'will_accept_vegan',
+            'will_accept_vegetarian',
+            'can_meet_for_delivery',
+            'delivery_details',
+            'available_days',
+            'available_time_periods',
+            'dairy_free',
+            'gluten_free',
+            'halal',
+            'low_carb',
+            'vegan',
+            'vegetarian',
+        )
