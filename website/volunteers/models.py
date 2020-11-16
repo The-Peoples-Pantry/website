82bdf18ad54762f8d1ffe60914d47fb3c6cf9428
Jenny Zhang <jennyzhang@mozilla.com>
2020-11-15 22:19:56 -0500

    Add initial model and stubs for Volunteers
    * Add model for Volunteer
    * Define routes and views for chef/driver sign-up
    * Create stub HTML files for vews

--- website/volunteers/models.py
+++ website/volunteers/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.conf import settings
+from recipients.models import Cities
 
-# Create your models here.
+class Volunteer(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True,
+        related_name="volunteer"
+    )
+    address_1 = models.CharField(
+        "Address line 1",
+        help_text="Street name and number",
+        max_length=settings.ADDRESS_LENGTH,
+        blank=True
+    )
+    address_2 = models.CharField(
+        "Address line 2",
+        help_text="Apartment, Unit, or Suite number",
+        max_length=settings.ADDRESS_LENGTH,
+        blank=True,
+    )
+    city = models.CharField(
+        "City",
+        max_length=settings.CITY_LENGTH,
+        choices=Cities.choices,
+        default=Cities.TORONTO,
+    )
+    postal_code = models.CharField(
+        "Postal code",
+        max_length=settings.POSTAL_CODE_LENGTH,
+        blank=True
+    )
+    is_coordinator = models.BooleanField("Volunteer Coordinator")
+    is_driver = models.BooleanField("Driver")
+    is_chef = models.BooleanField("Chef")
+    training_complete = models.BooleanField("Training Complete")
+
+    class Meta:
+        managed = False
+        db_table = 'person'
+
+
+# When user is created or saved, also save volunteer
+@receiver(post_save, sender=User)
+def save_volunteer(sender, instance, created, **kwargs):
+    if created:
+        Volunteer.objects.create(user=instance)
+    instance.volunteer.save()
--- website/volunteers/urls.py
+++ website/volunteers/urls.py
@@ -4,4 +4,8 @@ from . import views
 app_name = 'volunteers'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('chef-signup', views.ChefSignupView.as_view(), name='chef_signup'),
+    path('chef-list', views.ChefIndexView.as_view(), name='chef_list'),
+    path('delivery-signup', views.DeliverySignupView.as_view(), name='chef_signup'),
+    path('delivery-list', views.DeliveryIndexView.as_view(), name='chef_signup')
 ]
