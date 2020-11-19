a4dc2334f044bd47a894981b074c8ce0047ae1a1
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2020-11-18 22:13:49 -0500

    Register the volunteer application in the admin

--- website/volunteers/admin.py
+++ website/volunteers/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Volunteer
+from .models import Volunteer, VolunteerApplication
 
 admin.site.register(Volunteer)
+admin.site.register(VolunteerApplication)
