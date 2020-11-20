5e85ffeb82a1c798d1c89c4aa31fc8253209a349
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2020-11-19 20:25:34 -0500

    Add user profile view

--- website/core/views.py
+++ website/core/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import login
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 from django.urls import reverse_lazy
-from django.views.generic.edit import FormView
+from django.views.generic.edit import FormView, UpdateView
 
 from .forms import UserCreationForm
 
@@ -16,3 +18,13 @@ class UserCreationView(FormView):
         user = form.save()
         login(self.request, user)
         return super().form_valid(form)
+
+
+class UserProfileView(LoginRequiredMixin, UpdateView):
+    model = User
+    fields = ['first_name', 'last_name']
+    template_name = 'core/profile.html'
+    success_url = reverse_lazy('profile')
+
+    def get_object(self, queryset=None):
+        return self.request.user
