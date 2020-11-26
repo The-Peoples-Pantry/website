c72c0875a475012e55f753773ab0d3c900e98147
Eric Lindau <eric.lindau1@gmail.com>
2020-11-26 14:47:18 -0500

    Initial volunteer migration script with random passwords

---
+++ website/recipients/management/commands/migratevolunteers.py
@@ -0,0 +1,73 @@
+import csv
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+from django.db.utils import IntegrityError
+from volunteers.models import Volunteer
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def friendly_entry(entry: dict, required_fields):
+    return ', '.join(
+        map(
+            lambda field: entry[field],
+            required_fields
+        )
+    )
+
+
+def csv_to_django(entry: dict):
+    required_fields = ['Full name:', 'Email address:', 'Phone number:']
+    for field in required_fields:
+        if field not in entry or not entry[field]:
+            logging.warning(f'Required entry `{field}` is missing, skipping entry: {str(list(entry.values()))}')
+            return
+
+    try:
+        full_name = entry['Full name:'].split(' ')
+        first_name = full_name[0]
+        last_name = ' '.join(full_name[1:])
+        email = entry['Email address:']
+        user = User.objects.create(username=email, first_name=first_name, last_name=last_name, email=email)
+
+        random_password = User.objects.make_random_password()
+        user.set_password(random_password)
+
+        # NOTE: receiver enforces that volunteer object is created here as well
+        user.save()
+
+        # Volunteer fields
+        volunteer = Volunteer.objects.get(user=user)
+        volunteer.address_1 = entry['Full address (including any applicable apartment/unit/suite number):']
+        # NOTE: Full address is included in address_1; parsing would probably require geocoding query.
+        #       Likely better to stick with defaults in `address_2` and `postal_code` for now.
+        volunteer.phone_number = entry['Phone number:']
+        volunteer.save()
+
+    except KeyError as e:
+        raise KeyError(f'Entry is missing a required field {str(e)}: {friendly_entry(entry, required_fields)}')
+
+    except IntegrityError as e:
+        logging.warning(f'SKIPPING ENTRY: {str(e)} for entry: {friendly_entry(entry, required_fields)}')
+
+
+class Command(BaseCommand):
+    help = 'Migrate volunteers from PPT "Volunteers Directory" Google Sheet.'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--csv',
+            type=str,
+            help='Path to CSV export of Google Sheet with volunteer information.',
+            required=True
+        )
+
+    def handle(self, *args, **kwargs):
+        # dash replace with underscore
+        csv_path = kwargs['csv']
+
+        with open(csv_path) as csv_input:
+            reader = csv.DictReader(csv_input)
+            for entry in reader:
+                volunteer = csv_to_django(entry)
