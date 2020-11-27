e937d6927f264922cff1089309cf603575c47b1b
Jenny Zhang <jennyzhang@mozilla.com>
2020-11-27 12:42:29 -0500

    Hotfix missing redirect

--- website/public/views.py
+++ website/public/views.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.mixins import UserPassesTestMixin
 
 def index(request):
