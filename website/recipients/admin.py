08dc242ae2d49d3cb6d29ccbe15bbc3906a0e75d
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2020-11-26 20:28:14 -0500

    Add recipient admin tabs

--- website/recipients/admin.py
+++ website/recipients/admin.py
@@ -2,6 +2,53 @@ from django.contrib import admin
 
 from .models import MealRequest, UpdateNote, Delivery
 
-admin.site.register(MealRequest)
-admin.site.register(UpdateNote)
-admin.site.register(Delivery)
+
+class MealRequestAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'email',
+        'phone_number',
+        'created_at',
+        'delivery_date',
+    )
+    list_filter = (
+        'delivery_date',
+        'created_at',
+    )
+
+
+class UpdateNoteAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'note',
+        'request_id',
+        'created_at',
+    )
+    list_filter = (
+        'created_at',
+    )
+
+
+class DeliveryAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'request',
+        'status',
+        'chef',
+        'deliverer',
+        'pickup_start',
+        'pickup_end',
+        'dropoff_start',
+        'dropoff_end',
+        'container_delivery',
+    )
+    list_filter = (
+        'status',
+        'container_delivery',
+    )
+
+
+admin.site.register(MealRequest, MealRequestAdmin)
+admin.site.register(UpdateNote, UpdateNoteAdmin)
+admin.site.register(Delivery, DeliveryAdmin)
