e7daf425575c51f4cfd4437f7ba39b0cfa01de76
Jenny Zhang <jennyzhang@mozilla.com>
2020-11-26 21:10:47 -0500

    Extract out Address into own abstract model for easier sharing

--- website/volunteers/models.py
+++ website/volunteers/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.conf import settings
-from recipients.models import Cities
+from recipients.models import Cities, AddressModel
 
 
 def get_group_string(group_set):
@@ -23,7 +23,7 @@ class VolunteerRoles(models.TextChoices):
     ORGANIZERS = 'Organizers'
 
 
-class Volunteer(models.Model):
+class Volunteer(AddressModel):
     user = models.OneToOneField(
         User,
         on_delete=models.CASCADE,
