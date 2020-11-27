8f4262b6e259d3dc7bc72c90edbe00b62e497790
Jenny Zhang <jennyzhang@mozilla.com>
2020-11-27 12:54:01 -0500

    Add landline field and filter to requests admin

--- website/recipients/admin.py
+++ website/recipients/admin.py
@@ -12,9 +12,26 @@ class StatusFilter(admin.SimpleListFilter):
     def queryset(self, request, queryset):
         if self.value() is not None:
             matching_uuids = [delivery.request.uuid for delivery in Delivery.objects.filter(status=self.value())]
-            return queryset.filter(uuid__in=matching_uuids)
+            queryset = queryset.filter(uuid__in=matching_uuids)
 
         return queryset
+
+
+class LandlineFilter(admin.SimpleListFilter):
+    title = 'Phone type'
+    parameter_name = 'landline'
+
+    def lookups(self, request, model_admin):
+        return (
+            (False, 'Landline'),
+            (True, 'Cellphone')
+        )
+
+    def queryset(self, request, queryset):
+        if self.value():
+            queryset = queryset.filter(can_receive_texts=self.value())
+        return queryset
+
 
 
 class DeliveryInline(admin.TabularInline):
@@ -33,15 +50,18 @@ class MealRequestAdmin(admin.ModelAdmin):
         'name',
         'email',
         'phone_number',
+        'landline',
         'city',
         'created_at',
         'delivery_date',
         'status',
+        'landline',
     )
     list_filter = (
         'delivery_date',
-        'created_at',
         StatusFilter,
+        LandlineFilter,
+        'created_at',
     )
     inlines = (
         DeliveryInline,
@@ -50,6 +70,11 @@ class MealRequestAdmin(admin.ModelAdmin):
     actions = (
         'confirm',
     )
+
+
+    def landline(self, obj):
+        return 'No' if obj.can_receive_texts else 'Yes'
+    landline.short_description = "Landline"
 
     def confirm(self, request, queryset):
         confirmed_uuids = [delivery.request.uuid for delivery in Delivery.objects.filter(status=Status.DATE_CONFIRMED)]
