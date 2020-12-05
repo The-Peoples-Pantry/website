d73715976096469338c2586462160fe2b4df2757
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2020-12-05 10:33:00 -0500

    Remove get_volunteer templatetag
    We can use the .volunteer property on each of these objects

--- website/volunteers/templatetags/get_volunteer.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from volunteers.models import Volunteer
-
-register = template.Library()
-
-
-@register.filter(name='get_volunteer')
-def get_volunteer(user, prop):
-    return getattr(Volunteer.objects.get(user=user), prop)
