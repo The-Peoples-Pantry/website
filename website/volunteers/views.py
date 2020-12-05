800f94e7fe9eaf0de7567ccb716540704173f1cf
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2020-12-04 22:29:13 -0500

    Add sorting to the queryset

--- website/volunteers/views.py
+++ website/volunteers/views.py
@@ -28,7 +28,7 @@ class ChefSignupView(LoginRequiredMixin, GroupView, FormView, FilterView):
     form_class = ChefSignupForm
     permission_group = 'Chefs'
     filterset_class = ChefSignupFilter
-    queryset = MealRequest.objects.filter(delivery__isnull=True)
+    queryset = MealRequest.objects.filter(delivery__isnull=True).order_by('created_at')
 
     @property
     def success_url(self):
