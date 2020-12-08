b931dd1a65cd2a17db065c4972a5f1dfca4a6ac2
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2020-12-07 21:01:31 -0500

    Install sites app

--- website/website/settings.py
+++ website/website/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'django_extensions',
     'bootstrap4',
     'django_filters',
