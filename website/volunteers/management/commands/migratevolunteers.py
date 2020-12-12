ac2978e24e580a118e5f7ee767e32cd7c89ba57f
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2020-12-11 23:17:58 -0500

    Capture additional fields

--- website/volunteers/management/commands/migratevolunteers.py
+++ website/volunteers/management/commands/migratevolunteers.py
@@ -16,7 +16,12 @@ PHONE_FIELD = 'Phone number:'
 #       Likely better to stick with defaults in `address_2` and `postal_code` for now.
 ADDRESS_FIELD = 'Full address (including any applicable apartment/unit/suite number):'
 ROLES_FIELD = 'Would you like to...? Check all that apply.'
-REQUIRED_FIELDS = [NAME_FIELD, EMAIL_FIELD, ADDRESS_FIELD, PHONE_FIELD, ROLES_FIELD]
+PRONOUNS_FIELD = 'Preferred pronouns:'
+COOKING_PREFS_FIELD = 'What do you prefer to cook/bake? Check all that apply.'
+FOOD_TYPES_FIELD = 'What kind of meals/baked goods are you able to prepare? Check all that apply. '
+CLEANING_SUPPLIES_FIELD = 'Do you have cleaning supplies (soap, disinfectant, etc.) to clean your hands and kitchen?'
+PPE_FIELD = 'Do you have a mask you can use while cooking/baking and packaging food?'
+REQUIRED_FIELDS = [NAME_FIELD, EMAIL_FIELD, ADDRESS_FIELD, PHONE_FIELD]
 
 # Roles
 CHEF_INDICATOR = 'cook/bake'
@@ -85,6 +90,11 @@ class Command(BaseCommand):
         email = entry[EMAIL_FIELD]
         address = entry[ADDRESS_FIELD]
         phone_number = entry[PHONE_FIELD]
+        pronouns = entry[PRONOUNS_FIELD]
+        cooking_prefs = entry[COOKING_PREFS_FIELD]
+        food_types = entry[FOOD_TYPES_FIELD]
+        cleaning_supplies = entry[CLEANING_SUPPLIES_FIELD]
+        ppe = entry[PPE_FIELD]
         roles = entry[ROLES_FIELD].lower()
         first_name, last_name = self.split_name(full_name)
 
@@ -101,8 +111,16 @@ class Command(BaseCommand):
 
         # Volunteer fields
         user.volunteer.name = full_name
+        user.volunteer.email = email
         user.volunteer.address_1 = address
         user.volunteer.phone_number = phone_number
+        user.volunteer.pronouns = pronouns
+        user.volunteer.cooking_prefs = cooking_prefs
+        user.volunteer.food_types = food_types
+        if cleaning_supplies == 'Yes':
+            user.volunteer.have_cleaning_supplies = True
+        if ppe == 'Yes':
+            user.volunteer.have_ppe = True
         user.volunteer.save()
 
         # Role
