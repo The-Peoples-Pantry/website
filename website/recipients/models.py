8e2010583039f945734e7da72ad48408bc0a3453
Jenny Zhang <jennyzhang@mozilla.com>
2020-12-12 21:56:02 -0500

    Be clearer about language around meeting delivery drivedrs

--- website/recipients/models.py
+++ website/recipients/models.py
@@ -102,7 +102,7 @@ class HelpRequest(ContactInfo):
     # Information about the delivery
     can_meet_for_delivery = models.BooleanField(
         "Able to meet delivery driver",
-        help_text="We care about safety. Thus, we try to avoid delivery volunteers going into buildings or houses. Would you / the person requiring support be able to meet the delivery person in the lobby or door of the residence, while wearing protective equipment such as masks?",
+        help_text="Please confirm that you / the person requiring support will be able to meet the delivery person in the lobby or door of the residence, while wearing protective equipment such as masks?",
     )
     delivery_details = models.TextField(
         "Delivery details",
