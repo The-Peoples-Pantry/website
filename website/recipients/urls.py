9c71e19a52d1d4caf678636f8e9db61fb8aa0410
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2021-04-02 10:35:25 -0400

    Replace functional views with class based views
    Just for consistency

--- website/recipients/urls.py
+++ website/recipients/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 app_name = 'recipients'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.RequestIndexView.as_view(), name='index'),
     path('meal-request/new', views.MealRequestView.as_view(), name='new_meal_request'),
     path('grocery-request/new', views.GroceryRequestView.as_view(), name='new_grocery_request'),
-    path('success', views.success, name='success'),
+    path('success', views.RequestSuccessView.as_view(), name='success'),
     path('<int:pk>/detail', views.MealRequestDetail.as_view(), name='request_detail')
 ]
