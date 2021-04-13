d7d14366c8de1e6cac00413df65d953147fb1d83
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2021-04-13 17:06:14 -0400

    Use fallback image

--- website/core/templatetags/profile.py
+++ website/core/templatetags/profile.py
@@ -10,11 +10,14 @@ def profile_image(user):
     """
     Displays a profile photo for the mentioned user
     Pulls from Gravatar based on the user's email address
+    The "d" query param configures the fallback when no profile image is found
+
+    See http://en.gravatar.com/site/implement/images/
     """
     if user.is_anonymous:
         return None
 
     email_hash = hashlib.md5(user.email.encode('utf8')).hexdigest()
-    gravatar_url = f"https://www.gravatar.com/avatar/{email_hash}"
+    gravatar_url = f"https://www.gravatar.com/avatar/{email_hash}?d=mp"
     html = f'<img class="profile-image" alt="Profile image" src="{gravatar_url}">'
     return mark_safe(html)
