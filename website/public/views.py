65d69a35d6583cfd6569c5f19ffa8dc4ea9bef43
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2021-04-28 16:38:07 -0400

    Switch to fluid carousel of many volunteer photos

--- website/public/views.py
+++ website/public/views.py
@@ -1,5 +1,15 @@
+import random
 from datetime import date
 from django.views.generic import TemplateView
+from django.contrib.staticfiles.storage import staticfiles_storage
+
+
+def random_images(directory, count=None):
+    """Choose $count random images from $directory, return the paths to be used with static"""
+    _subdirectories, filenames = staticfiles_storage.listdir(directory)
+    paths = [f"{directory}/{filename}" for filename in filenames]
+    random.shuffle(paths)
+    return random.sample(paths, k=count) if count else paths
 
 
 class IndexView(TemplateView):
@@ -120,16 +130,88 @@ class MediaView(TemplateView):
 
 class AboutView(TemplateView):
     template_name = "public/about.html"
+    VOLUNTEER_PHOTOS = [
+        "photos/chef_volunteers/img-20210402-wa0000.jpg",
+        "photos/chef_volunteers/img_2398.jpg",
+        "photos/chef_volunteers/image_from_ios-42.jpg",
+        "photos/chef_volunteers/img_2399.jpg",
+        "photos/chef_volunteers/img_2416.jpg",
+        "photos/chef_volunteers/20210207_092201.jpg",
+        "photos/chef_volunteers/img_2417.jpg",
+        "photos/chef_volunteers/IMG_20210125_122959359.jpg",
+        "photos/chef_volunteers/20210127_084539.jpg",
+        "photos/chef_volunteers/img_2411.jpg",
+        "photos/chef_volunteers/img_2405.jpg",
+        "photos/chef_volunteers/img-20210402-wa0004.jpg",
+        "photos/chef_volunteers/image_from_ios-4.jpg",
+        "photos/chef_volunteers/IMG_2067.JPG",
+        "photos/chef_volunteers/1275ba35-e885-4d59-9ec7-6e81d96431c6_1_105_c.jpeg",
+        "photos/chef_volunteers/IMG_53871.jpg",
+        "photos/chef_volunteers/IMG_2065.JPG",
+        "photos/chef_volunteers/image_from_ios-36.jpg",
+        "photos/chef_volunteers/image_from_ios-22.jpg",
+        "photos/chef_volunteers/IMG_10791.jpg",
+        "photos/chef_volunteers/img_2851.jpg",
+        "photos/chef_volunteers/image7.jpeg",
+        "photos/chef_volunteers/img_2263.jpg",
+        "photos/chef_volunteers/image_from_ios-27.jpg",
+        "photos/chef_volunteers/image_from_ios-3.jpg",
+        "photos/chef_volunteers/img_2671.jpg",
+        "photos/chef_volunteers/IMG_20210227_123814_400.jpg",
+        "photos/chef_volunteers/image_from_ios-19.jpg",
+        "photos/chef_volunteers/img_2260.jpg",
+        "photos/chef_volunteers/img_2880.jpg",
+        "photos/chef_volunteers/img_2325.jpg",
+        "photos/chef_volunteers/IMG_20210324_090837_207.jpg",
+        "photos/chef_volunteers/img_2332.jpg",
+        "photos/chef_volunteers/img_2326.jpg",
+        "photos/chef_volunteers/IMG_2247.JPG",
+        "photos/chef_volunteers/img_2333.jpg",
+        "photos/chef_volunteers/img_2455.jpg",
+        "photos/chef_volunteers/img_2451.jpg",
+        "photos/chef_volunteers/img_2337.jpg",
+        "photos/chef_volunteers/img_2257.jpg",
+        "photos/chef_volunteers/IMG_2068.jpg",
+        "photos/chef_volunteers/img_2322.jpg",
+        "photos/chef_volunteers/img_2320.jpg",
+        "photos/chef_volunteers/img_2334.jpg",
+        "photos/chef_volunteers/20200928_172633.jpg",
+        "photos/chef_volunteers/img_2420.jpg",
+        "photos/chef_volunteers/20210204_152223.jpg",
+        "photos/chef_volunteers/image-1.png",
+        "photos/chef_volunteers/img_2423.jpg",
+        "photos/chef_volunteers/image_from_ios-59.jpg",
+        "photos/chef_volunteers/image3.jpeg",
+        "photos/delivery_volunteers/tppmaximg_3163.jpg",
+        "photos/delivery_volunteers/tppimg_3167.jpg",
+        "photos/delivery_volunteers/20210109_110836.jpg",
+        "photos/delivery_volunteers/20201104_192418.jpg",
+        "photos/delivery_volunteers/20210124_161154.jpg",
+        "photos/delivery_volunteers/20210322_164028.jpg",
+        "photos/delivery_volunteers/img_20210327_155559.jpg",
+        "photos/delivery_volunteers/IMG_2516.JPG",
+        "photos/delivery_volunteers/img_2851.jpg",
+        "photos/delivery_volunteers/20210201_164929.jpg",
+        "photos/delivery_volunteers/IMG_2492.JPG",
+        "photos/delivery_volunteers/20210321_154951.jpg",
+        "photos/delivery_volunteers/IMG_2490.JPG",
+        "photos/delivery_volunteers/20210124_155001.jpg",
+        "photos/delivery_volunteers/20210125_163325.jpg",
+        "photos/delivery_volunteers/20210321_161025.jpg",
+        "photos/delivery_volunteers/People_s Pantry delivery.jpg",
+        "photos/delivery_volunteers/20210131_155054.jpg",
+        "photos/delivery_volunteers/IMG_2340.JPG",
+        "photos/delivery_volunteers/img_2341.jpg",
+        "photos/organizers/yann-eli.jpg",
+        "photos/organizers/jade.jpg",
+        "photos/organizers/michelle.jpg",
+        "photos/organizers/andrea.jpg",
+    ]
 
     @property
     def extra_context(self):
         return {
-            'testimonials': [
-                'testimonials/crystal.png',
-                'testimonials/jan.png',
-                'testimonials/liz.png',
-                'testimonials/wanyi.png',
-            ]
+            'photos': random.sample(self.VOLUNTEER_PHOTOS, k=12),
         }
 
 
