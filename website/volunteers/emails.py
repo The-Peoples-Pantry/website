115e3fa7de48159820c720e8a5f060abb20c7b1c
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2021-06-06 18:26:42 -0400

    Add volunteer outreach email for chefs

--- website/volunteers/emails.py
+++ website/volunteers/emails.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.urls import reverse
 from website.emails import Email
 
 
@@ -14,3 +15,16 @@ class VolunteerApplicationApprovalEmail(Email):
     template = "emails/volunteer_applications/approval.html"
     reply_to = settings.VOLUNTEER_COORDINATORS_EMAIL
     include_unsubscribe_link = False
+
+
+class VolunteerOutreachChefSignupsEmail(Email):
+    subject = "Chefs needed for The People's Pantry!"
+    template = "emails/volunteer_outreach/chef_signups.html"
+    reply_to = settings.VOLUNTEER_COORDINATORS_EMAIL
+    include_unsubscribe_link = True
+
+    @property
+    def extra_context(self):
+        return {
+            'call_to_action_url': f"https://www.thepeoplespantryto.com{reverse('volunteers:chef_signup_list')}",
+        }
