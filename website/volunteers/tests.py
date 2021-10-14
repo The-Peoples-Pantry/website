765895b596c671d1820bc05cf406d1fa5c1f87c8
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2021-10-14 19:24:08 -0400

    Add an authenticated test that requires a user login

--- website/volunteers/tests.py
+++ website/volunteers/tests.py
@@ -1,3 +1,16 @@
-# from django.test import TestCase
+from django.urls import reverse
+from django.test import TestCase
 
-# Create your tests here.
+from website.test_helper import TestHelper
+
+
+class ChefSignupListViewTest(TestHelper, TestCase):
+    def test_something(self):
+        self.with_user(groups=["Chefs"])
+        url = reverse("volunteers:chef_signup_list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(
+            response,
+            "Sign up to cook meals",
+        )
