de1993d63f42d30735bc8b9ca435184264fa4cde
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2021-10-14 19:18:13 -0400

    Create a simple test for a public view

--- website/public/tests.py
+++ website/public/tests.py
@@ -1,3 +1,13 @@
-# from django.test import TestCase
+from django.urls import reverse
+from django.test import TestCase
 
-# Create your tests here.
+
+class IndexViewTest(TestCase):
+    def test_something(self):
+        url = reverse("public:index")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(
+            response,
+            "We provide home cooked meals and groceries to those who have been hit hardest by COVID-19",
+        )
